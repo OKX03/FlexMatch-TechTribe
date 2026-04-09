5564c4c42a72db22c480ec8a7190a6d65be2677f
Oh Kai Xuan <ohkaixuan1@gmail.com>
2026-04-09 22:44:28 +0800

    Remove Sql

--- sql/notification.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE notification (
-    notiID INT AUTO_INCREMENT PRIMARY KEY,
-    userID VARCHAR(10),  -- Admin userID
-    notificationText TEXT,
-    notificationType ENUM('status-change', 'warning') DEFAULT 'status-change',
-    isRead BOOLEAN DEFAULT FALSE,
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (userID) REFERENCES login(userID)
-);
